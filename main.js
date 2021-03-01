0afa0bb020a589f78bd758a6d77ee0b2af17e2f8
kngmlnr <73819914+kngmlnr@users.noreply.github.com>
2021-03-01 10:23:49 +0100

    kinga.
    new js

--- main.js
+++ main.js
@@ -31,3 +31,67 @@ illustrations.forEach(illustration => {
     })
 })
 
+
+// VIDEO TEXT FADE IN
+
+function scrollAppear() {
+    var textFade = document.querySelector('.text_fade')
+    var textPosition = textFade.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.3;
+
+    if (textPosition < screenPosition) {
+        textFade.classList.add('text_appear');
+    }
+}
+
+window.addEventListener('scroll', scrollAppear);
+
+function scrollAppear2() {
+    var textFade2 = document.querySelector('.text_fade2')
+    var textPosition2 = textFade2.getBoundingClientRect().top;
+    var screenPosition2 = window.innerHeight / 1.3;
+
+    if (textPosition2 < screenPosition2) {
+        textFade2.classList.add('text_appear2');
+    }
+}
+
+window.addEventListener('scroll', scrollAppear2);
+
+
+
+//VIDEO 
+
+let myVideo = document.getElementById('myvideo');
+
+function playPause() {
+    if (myVideo.paused) {
+        myVideo.play();
+        playpause.style.opacity = '0';
+        myVideo.controls = true;
+    }
+    else {
+        myVideo.pause();
+        playpause.style.opacity = '1';
+        myVideo.controls = false;
+    }
+}
+
+playpause.addEventListener('click', playPause);
+
+
+let myVideo2 = document.getElementById('myvideo2');
+myVideo2.controls = true;
+
+function playPause2() {
+    if (myVideo2.paused) {
+        myVideo2.play();
+        playpause2.style.opacity = '0';
+    }
+    else {
+        myVideo2.pause();
+        playpause2.style.opacity = '1';
+    }
+}
+
+playpause2.addEventListener('click', playPause2);
